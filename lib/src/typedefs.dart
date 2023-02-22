e207f7d7a55c1cdc90abec6167c317394adfc0c8
Tim Logemann <tim@todaytomorrow.nl>
2023-02-22 14:03:17 +0100

    add more type definitions

--- lib/src/typedefs.dart
+++ lib/src/typedefs.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_card_swiper/src/enums.dart';
+
+import 'model_properties.dart';
 
 typedef CardSwiperOnSwipe = void Function(
   int index,
@@ -8,3 +11,14 @@ typedef CardSwiperOnSwipe = void Function(
 typedef CardSwiperOnEnd = void Function();
 
 typedef CardSwiperOnTapDisabled = void Function();
+
+typedef CardSwiperCardBuilder = Widget Function(
+  BuildContext context,
+  Widget child,
+);
+
+/// Builder for displaying an overlay on the most foreground card.
+typedef CardSwiperOverlayBuilder = Widget Function(
+  BuildContext context,
+  OverlaySwipeProperties swipeProperty,
+);
